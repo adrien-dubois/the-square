92c82ee7071a5a39f147e18e5bd79b82a67eb6ab
adrien-dubois <mcsnoos@gmail.com>
2022-01-27 23:51:32 +0100

    [WIP]AboutSection

--- src/component/AboutSec/AboutSec.elements.js
+++ src/component/AboutSec/AboutSec.elements.js
@@ -5,10 +5,66 @@ export const AboutSection = styled.div`
     width: 100vw;
     position: relative;
     display: flex;
+    flex-direction: column;
     justify-content: center;
     align-items: center;
 `;
 
 export const Waves = styled.img`
+    width: 100%;
+`;
+
+export const Main = styled.div`
+    margin: 0 15rem;
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+`;
+
+export const Title = styled.h1`
+    display: inline-block;
+    font-size: 2rem;
+`;
+
+export const CurvedLine = styled.div`
+    width: 7rem;
+    height: 2rem;
+    border: 5px solid var(--teal-blue);
+    border-color: var(--teal-blue) transparent transparent transparent;
+    border-radius: 150%/60px 70px 0 0;
+`;
+
+export const Content = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+`;
+
+export const Rocket = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 40%;
+    padding-bottom: 5rem;
+`;
+
+export const AboutText = styled.div`
+    position: relative;
+    width: 50% ;
+`;
+
+export const Astronaut = styled.div`
+    width: 50%;
+    margin-bottom: 3rem;
+    position: absolute;
+    right: 0;
+    bottom: 100%;
+`;
+
+export const Text = styled.h4`
+
+`;
+
+export const Circle = styled.span`
 
 `;
--- src/component/AboutSec/AboutSec.js
+++ src/component/AboutSec/AboutSec.js
@@ -2,13 +2,48 @@ import React from 'react';
 import wave from "../../assets/img/wave.svg";
 import { 
     AboutSection, 
+    AboutText, 
+    Astronaut, 
+    Circle, 
+    Content, 
+    CurvedLine, 
+    Main, 
+    Rocket, 
+    Text, 
+    Title, 
     Waves 
 } from './AboutSec.elements';
+import rocket from '../../assets/img/rocket.svg';
+import astronaut from '../../assets/img/astronaut.svg';
 
 const AboutSec = () => {
   return (
     <AboutSection>
         <Waves src={wave} alt="waves" />
+        <Main>
+            <div>
+                <Title>Le Collectif</Title>
+                <CurvedLine />
+            </div>
+            <Content>
+                <Rocket>
+                    <img src={rocket} alt='Rocket' width='400' height='400' /> 
+                </Rocket>
+                <AboutText>
+                    <Astronaut>
+                    <img src={astronaut} alt='Astronaut' width='300' height='300' />
+                    </Astronaut>
+                    <Text>
+                        Fugiat aute elit laborum adipisicing qui id aliqua do est dolor. Enim laboris in tempor occaecat fugiat eiusmod. Pariatur exercitation minim quis id proident nisi duis ut minim. Ipsum irure ullamco sunt adipisicing nulla aliqua nostrud magna veniam mollit. Deserunt proident excepteur nostrud magna consectetur in quis sunt do. Pariatur ex aliqua incididunt qui adipisicing sit voluptate id.
+                    </Text>
+                    <div>
+                        <Circle />
+                        <Circle />
+                        <Circle />
+                    </div>
+                </AboutText>
+            </Content>
+        </Main>
     </AboutSection>
   );
 };
